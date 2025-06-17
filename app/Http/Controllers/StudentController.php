349c7b3951e442a04809282b8e7e3db6aa3195fd
UDD L301 PC-26 <karlapagkalinawan@gmail.com>
2025-06-17 15:15:00 +0800

    pinaka-latest

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,17 +9,13 @@ class StudentController extends Controller
 {
     public function index()
     {
-       // $data['students'] = [
-       // ['name' => 'Juan Dela Cruz'],
-       // ['name' => 'Crisostomo Ibarra'],
-        //['name' => 'Captain']
-
-  //  ];
-        $data['students']= Student::orderBy('created_at', 'desc')->paginate(15);
+        $data['students'] = Student::orderBy('created_at', 'desc')->paginate(15);
         $data['isAdmin'] = true;
         $data['user'] = "Karla";
+
         return view('students', $data);
     }
+
     public function create()
     {
         return view('students.create');
@@ -27,45 +23,77 @@ class StudentController extends Controller
 
     public function store(Request $request)
     {
-       $request->validate(
-        [
-            'fname'    => 'required',
-            'lname'    => 'required',
-            'email'    => 'required|email|unique:students,email',
-            'contact_number'   => 'required'
-        ],
-        [
-            'fname.required' => 'The first name field is required.',
-            'lname.required' => 'The last name field is required.',
-        ]
-    );
+        $request->validate(
+            [
+                'fname'           => 'required',
+                'lname'           => 'required',
+                'email'           => 'required|email|unique:students,email',
+                'contact_number'  => 'required',
+            ],
+            [
+                'fname.required'  => 'The first name field is required.',
+                'lname.required'  => 'The last name field is required.',
+            ]
+        );
 
-    Student::create([
-        'fname' => $request['fname'],
-        'lname' => $request['lname'],
-        'email' => $request['email'],
-        'contact_number' => $request['contact_number'],
-       ]);
+        Student::create([
+            'fname'          => $request->input('fname'),
+            'lname'          => $request->input('lname'),
+            'email'          => $request->input('email'),
+            'contact_number' => $request->input('contact_number'),
+        ]);
 
-       return redirect()->to('students');
+        return redirect()->to('students')->with('success', 'Student has been added successfully.');
     }
-    public function edit ($id)
+
+    public function edit($id)
     {
-        return "The id is $id";
+        $data['student'] = Student::find($id);
+        return view('students.edit', $data);
     }
-    public function update(Request $id)
-    {
 
+    public function update(Request $request, $id)
+    {
+        $request->validate(
+            [
+                'fname'           => 'required',
+                'lname'           => 'required',
+                'email'           => 'required|email|unique:students,email,' . $id,
+                'contact_number'  => 'required',
+            ],
+            [
+                'fname.required'  => 'The first name field is required.',
+                'lname.required'  => 'The last name field is required.',
+            ]
+        );
+
+        $student = Student::find($id);
+
+        if ($student) {
+            $student->fname           = $request->input('fname');
+            $student->lname           = $request->input('lname');
+            $student->email           = $request->input('email');
+            $student->contact_number  = $request->input('contact_number');
+            $student->save();
+        }
+
+        return redirect()->to('students')->with('success', 'Student has been added successfully.');
     }
-    public function show ($id)
+
+    public function show($id)
     {
         return "show id $id";
     }
+
     public function destroy($id)
     {
 
+        $student = Student::find($id);
+
+        if ($student) {
+            $student->delete();
+        }
+
+        return redirect()->back()->with('success', 'Student has been added successfully.');
     }
-
 }
-
-
--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -2,28 +2,61 @@
 
 @section('content')
     <h2 class="students">{{ $user }}</h2>
-
+    @session('success')
+    </div>
+    <div class="alert alert-success" role="alert">
+        {{ session('success') }}
+    </div>
+    @endsession
     @if($isAdmin)
-        <ul>
-            @foreach ($students as $key => $student)
-                <li style="font-size: 20px">{{ $key + 1 }}. {{ $student->fname . ' ' . $student->lname}}</li>
-            @endforeach  
+    <div class="d-flex justify-content-end">
+        <a href="{{ url('students/create') }}" class="btn btn-primary">Create Student</a>
+    </div>
+        <table class="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Student Name</th>
+                    <th>Email</th>
+                    <th>Contact Number</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($students as $key => $student)
+                    <tr>
+                        <td>{{ $key + 1 }}.</td>
+                        <td>{{ $student->fname . ' ' . $student->lname }}</td>
+                        <td>{{ $student->email }}</td>
+                        <td>{{ $student->contact_number }}</td>
+                        <td>
+                            <a href="{{ url('students', $student->id) }}/edit" class="btn btn-success btn-sm">Edit</a>
 
-            {!! $students->links() !!}
-        </ul>    
+                            <form action="{{ url('students', $student->id) }}" method="POST" style="display:inline-block;" 
+                                  onsubmit="return confirm('Are you sure you want to delete this student?');">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        {!! $students->links() !!} 
     @else
         <h1>Admin is False</h1>
     @endif
 @endsection
 
-@section ('css')
+@section('css')
     <style>
         .students {
             text-align: center;
         }
     </style>
-@endsection 
+@endsection
 
 @push('scripts')
-
 @endpush
